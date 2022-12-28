0ade19ad7f158d4baf8efaf8076409abd856fd56
khushiBvm <khushiranpariya.bvminfotech@gmail.com>
2022-12-28 17:53:48 +0530

    add dropdown.

--- resources/views/frontend/layout/frontend-auth.blade.php
+++ resources/views/frontend/layout/frontend-auth.blade.php
@@ -113,7 +113,7 @@
 
 <script src="{{asset('frontend/assets/js/libs/isotope/isotope.pkgd.min.js')}}"></script>
 
-<script src="{{asset('frontend/assets/js/libs/select2/select2.min.js')}}"></script>
+{{--<script src="{{asset('frontend/assets/js/libs/select2/select2.min.js')}}"></script>--}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
       integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
       crossorigin="anonymous" referrerpolicy="no-referrer"/>
